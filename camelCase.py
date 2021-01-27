67803f7132141d5ed0a135aa6217c51af9e7d97d
kelseystiff <kelsey>
2021-01-27 11:44:50 -0600

    added banner feature

--- camelCase.py
+++ camelCase.py
@@ -1,18 +1,21 @@
 def camelcase(sentence):
-	""" Convert sentence to camelCase, for example, "Display all books" 
-	is converted to "displayAllBooks" """
+    title_case = sentence.title() #uppercase
+    upper_camel_cased = title_case.replace(' ', '')
+    return upper_camel_cased[0:1].lower() + upper_camel_cased
 
-    title_case = sentence.title() # Uppercase first letter of each word
-	upper_camel_cased = title_case.replace(' ', '') # remove spaces 
-	# Lowercase first letter, join with rest of string 
-	# Slices don't produce index out of bounds errors. 
-	# So this still works on empty strings, strings of length 1
-	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
-	def main():
-	sentence = input('Enter your sentence: ')
-	output = camelcase(sentence)
-	print(output)
+def banner():
+    """ Display welcome Banner """
+    message = "CAMELCASE PROGRAM"
+    stars = '*' * len(message)
+    print(f'{stars}\n{message}\n{stars}')
 
-    
-if __name__ == '__main__':
-	main()
+def main():
+    banner()
+    sentence = input('Enter your sentence: ')
+    output = camelcase(sentence)
+    print(output)
+
+
+
+main()
+
